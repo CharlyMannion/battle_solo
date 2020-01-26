457c9ccba8ac9b94ce57586848512b5684c5e7f9
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-26 15:47:39 +0000

    refactors player spec to use reusable test mock:

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,14 +2,13 @@ require 'player'
 
 describe Player do
   DEFAULT_HIT_POINTS = 100
+  subject(:test_player) { Player.new("Kitty")}
 
   describe '#initialize' do
     it 'should have default hit points' do
-      test_player = Player.new("Kitty")
       expect(test_player.hit_points).to eq(DEFAULT_HIT_POINTS)
     end
     it 'should set the name of the player at creation' do
-      test_player = Player.new("Kitty")
       expect(test_player.name).to eq("Kitty")
     end
   end
