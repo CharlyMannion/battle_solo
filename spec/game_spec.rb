f0dd454f2ecaafa4b4f093d068abea9c3b8d3728
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-27 10:46:09 +0000

    refactors game

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -21,18 +21,4 @@ describe Game do
       game.attack(player_jim)
     end
   end
-
-  # describe '#player_1' do
-  #   it 'creates a player object within game' do
-  #     # game.set_player_1(player_kitty)
-  #     expect(game.player_1).to eq(player_kitty)
-  #   end
-  # end
-  #
-  # describe '#player_2' do
-  #   it 'creates a player object within game' do
-  #     # game.set_player_2(player_jim)
-  #     expect(game.player_2).to eq(player_jim)
-  #   end
-  # end
 end
