3d936f75499d128c9096d28cabd7b61b13c796f1
Charlotte Mannion <charlottemannion100@gmail.com>
2020-01-27 10:39:10 +0000

    refactors game

--- lib/game.rb
+++ lib/game.rb
@@ -7,20 +7,9 @@ class Game
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
-  # def initialize(player_class = Player)
-    # @player_class = player_class
   end
 
   def attack(player)
     player.receive_attack
   end
-
-  # def set_player_1(player)
-  #   @player_1 = player
-  # end
-  #
-  # def set_player_2(player_two)
-  #   @player_2 = player_two
-  # end
-
 end
